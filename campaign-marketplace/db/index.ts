e1a0fced83923c35a9c27e12fd1fbea0437fe2a6
Ekin Aslan <ekinaslan.js@gmail.com>
2026-09-05 14:12:09 +0300

    Dockerise

--- campaign-marketplace/db/index.ts
+++ campaign-marketplace/db/index.ts
@@ -3,11 +3,12 @@ import postgres from "postgres";
 
 const user = process.env.POSTGRES_USER || "wayv_service";
 const password = process.env.POSTGRES_PASSWORD || "Yvd@BKWZ=WR+Eh0H";
+const host = process.env.POSTGRES_HOST || "localhost";
 const port = process.env.POSTGRES_PORT || "5432";
 const db_name = process.env.POSTGRES_DB || "campaign_marketplace";
 
 export const db = drizzle(
   postgres(
-    `postgresql://${user}:${encodeURIComponent(password)}@localhost:${port}/${db_name}`,
+    `postgresql://${user}:${encodeURIComponent(password)}@${host}:${port}/${db_name}`,
   ),
 );
--- campaign-marketplace/drizzle.config.ts
+++ campaign-marketplace/drizzle.config.ts
@@ -7,6 +7,6 @@ export default defineConfig({
   out: `${path}/migrations`,
   dialect: "postgresql",
   dbCredentials: {
-    url: `postgresql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@localhost:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}`,
+    url: `postgresql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST || "localhost"}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}`,
   },
 });
